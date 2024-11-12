df21efcc68fa60aed2b1c32eca5fd6cc6ef44751
Alexey A. Tsukanov <a.a.tsukanov@yandex.ru>
2024-11-12 17:56:33 +0300

    Добавлена задача 16.1 (FastAPI, uvicorn, /docs)

--- module_16_1.py
+++ module_16_1.py
@@ -23,3 +23,4 @@ async def news(username: str, age: int):
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000, log_level='info')
+    # ПОТОМ ЗАЙТИ НА docs: http://127.0.0.1:8000/docs
